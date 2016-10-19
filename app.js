3943908cffff7700aaaa1e5af13097fcbaff7983
Suresh Shrestha <sureshshrestha@lftechnology.com>
2016-10-19 08:20:27 +0545

    Student crud done.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 // var users = require('./routes/users');
-var todos = require('./routes/todos');
+var students = require('./routes/students');
 
 var app = express();
 
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 // app.use('/users', users);
-app.use('/todos', todos);
+app.use('/students', students);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -63,7 +63,7 @@ var mongoose = require('mongoose');
 // Use native Node promises
 mongoose.Promise = global.Promise;
 // connect to MongoDB
-mongoose.connect('mongodb://localhost/todo-api')
+mongoose.connect('mongodb://localhost/student-mgmt-system-api')
   .then(() =>  console.log('connection succesful'))
   .catch((err) => console.error(err));
 
